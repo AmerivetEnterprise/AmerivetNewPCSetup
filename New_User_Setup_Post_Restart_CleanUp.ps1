668424b57dcca6c1b0633a10004108fa6abfed3b
amerivet <it@amerivet.com>
2024-08-01 13:09:47 -0500

    Add files via upload

---
+++ New_User_Setup_Post_Restart_CleanUp.ps1
@@ -0,0 +1,21 @@
+﻿#Version 5.0 
+#08/01/2024
+
+$host.UI.RawUI.WindowTitle = "New User Setup Cleanup"
+
+Write-Host 'Script will continue in 10 seconds'
+Start-Sleep 10
+
+Remove-Item -Path "C:\IT\New_User_Setup_Post_Restart_1.ps1" -Force
+Remove-Item -Path "C:\IT\New_User_Setup_Post_Restart_2.ps1" -Force 
+Remove-Item -Path "C:\IT\New_User_Setup_Post_Restart_3.ps1" -Force
+Remove-Item -Path "C:\IT\New_User_Setup_Post_Restart_4.ps1" -Force
+Remove-Item -Path "C:\IT\New_User_Setup_Post_Restart_5.ps1" -Force
+
+Write-Host "Script Cleanup Complete" -foregroundcolor "Green"
+
+Add-Content -Path C:\IT\Complete.txt -value "`Cleanup - Complete"
+
+Cleanmgr
+
+Read-Host "All Scripts Complete"

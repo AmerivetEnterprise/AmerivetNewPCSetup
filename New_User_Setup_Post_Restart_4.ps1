bd3673e9ba2ed938b60b4c6fe1d2cbfc139e47d3
amerivet <it@amerivet.com>
2024-08-09 12:38:14 -0500

    Update New_User_Setup_Post_Restart_4.ps1

--- New_User_Setup_Post_Restart_4.ps1
+++ New_User_Setup_Post_Restart_4.ps1
@@ -25,6 +25,11 @@ $OS_Key = $OS_Key.OA3xOriginalProductKey
 slmgr.vbs /ipk $OS_Key
 
 ##################################################################################################################################################################
+
+#Stops user from opening old outlook
+Rename-Item -Path 'C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE' -NewName "OUTLOOK.EXE.BAK" 
+
+##################################################################################################################################################################
 # Busylight Software install 
 
 $input = Read-Host "Install Busylight Software (HQ Employee's only) [y/n]"
